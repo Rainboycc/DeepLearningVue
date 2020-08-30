b052165bf905631f70895a8404d7bcae380bc178
rainboycc <1306463032@qq.com>
2020-08-30 23:21:24 +0800

    add regexp string

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,3 +1,10 @@
+// 模板采用正则表达式进行解析，生成ast语法树
+const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
+const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z${unicodeRegExp.source}]*`
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`
+const startTagOpen = new RegExp(`^<${qnameCapture}`)
+const startTagClose = /^\s*(\/?)>/
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`)
 // ast语法树 用对象来描述原生语法的 虚拟DOM 用对象来描述DOM节点的
 export function compileToFunction (template) {
   console.log(template)
